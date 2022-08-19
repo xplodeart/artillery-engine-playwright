eaabc207543a809c5f9124adeea098be6ea457a2
Marjan <max.xplodeart@gmail.com>
2022-08-19 10:16:58 +0200

    tweaks

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ class PlaywrightEngine {
     return async function scenario(initialContext, cb) {
       events.emit("started");
       const browser = await chromium.launch({
-        //headless:
-        //self.config.engines.playwright.headless === false ? false : true,
-        headless: false,
+        headless:
+          self.config.engines.playwright.headless === false ? false : true,
+        //headless: false,
         ignoreDefaultArgs: ["--mute-audio"],
         args: [
           "--enable-precise-memory-info",
-          "--no-sandbox",
+          //"--no-sandbox",
           "--autoplay-policy=no-user-gesture-required",
           "--use-fake-ui-for-media-stream",
           "--use-fake-device-for-media-stream",
